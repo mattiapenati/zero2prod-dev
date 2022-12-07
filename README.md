336770dbbebc313d01cc1850422be876ee580286
Mattia Penati <mattia.penati@protonmail.com>
2022-12-07 21:23:57 +0100

    3.3 - our first endpoint: a basic health check

--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Zero To Production (with axum)
 
 Developed following the book [Zero To Production In Rust](https://www.zero2prod.com/) using [axum](https://crates.io/crates/axum).
+
+
+A summary of the differences:
+  - **§3.3**: the chosen web framework is [axum](https://crates.io/crates/axum).
+
--- src/main.rs
+++ src/main.rs
@@ -1,1 +1,17 @@
-fn main() {}
+use axum::{routing, Router};
+use hyper::StatusCode;
+
+#[tokio::main]
+async fn main() -> hyper::Result<()> {
+    hyper::Server::bind(&"127.0.0.1:8000".parse().expect("invalid bind address"))
+        .serve(
+            Router::new()
+                .route("/health_check", routing::get(health_check))
+                .into_make_service(),
+        )
+        .await
+}
+
+async fn health_check() -> StatusCode {
+    StatusCode::NO_CONTENT
+}
