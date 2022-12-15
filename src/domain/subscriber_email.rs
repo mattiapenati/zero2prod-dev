cd10fe355572bfc34bb16ae6b60aab7db4614160
Mattia Penati <mattia.penati@protonmail.com>
2022-12-15 22:43:25 +0100

    6.13 - property-based testing

--- src/domain/subscriber_email.rs
+++ src/domain/subscriber_email.rs
@@ -23,6 +23,7 @@ impl AsRef<str> for SubscriberEmail {
 mod tests {
     use super::SubscriberEmail;
     use claym::assert_err;
+    use fake::{faker::internet::en::SafeEmail, Fake};
 
     #[test]
     fn empty_string_is_rejected() {
@@ -40,5 +41,22 @@ mod tests {
     fn email_missing_subject_is_rejected() {
         let email = "@domain.com".to_string();
         assert_err!(SubscriberEmail::parse(email));
+    }
+
+    #[derive(Debug, Clone)]
+    struct ValidEmailFixture(String);
+
+    impl quickcheck::Arbitrary for ValidEmailFixture {
+        fn arbitrary<G: quickcheck::Gen>(g: &mut G) -> Self {
+            let email = SafeEmail().fake_with_rng(g);
+            Self(email)
+        }
+    }
+
+    #[quickcheck_macros::quickcheck]
+    fn valid_emails_are_parsed_successfully(
+        ValidEmailFixture(valid_email): ValidEmailFixture,
+    ) -> bool {
+        SubscriberEmail::parse(valid_email).is_ok()
     }
 }
